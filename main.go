9fbc0bbcdea758fc4f40f2ce5178490c75815cfc
lekan <lekan.lekan@gmail.com>
2022-06-21 23:59:01 +0300

    like mac

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
 	"strings"
 )
 
@@ -31,10 +32,54 @@ func execInput(input string) error {
 	return cmd.Run()
 }
 
+func getUsername() (string, error) {
+	user, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return user.Username, nil
+
+}
+
+func getHostname() (string, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	machinename := strings.Split(hostname, ".")
+	return machinename[0], nil
+}
+
+func getCurrentDir() (string, error) {
+	fullPath, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	dir := strings.Split(fullPath, "/")
+
+	return dir[len(dir)-1], nil
+}
+
 func main() {
+	user, err := getUsername()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	hostname, err := getHostname()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	dir, err := getCurrentDir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("> ")
+		fmt.Printf("%s@%s %s %% ", user, hostname, dir)
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
